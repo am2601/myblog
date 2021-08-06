6d1a05038fdb66a28c6f5385979db7aa8125f920
am2601 <artem.markovskiy2003@gmail.com>
2021-08-06 22:00:34 +0200

    some fix in previous commit

---
+++ accounts/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+
+
+def registrate_user(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.save()
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/registration.html', {'form': form})
+
+
+def login_user(request):
+    if request.method == "POST":
+        # form = AuthenticationForm(request.POST)
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        login(request, user)
+        if user is not None:
+            # login(request, user)
+            return redirect('post_list')
+        else:
+            return render(request, 'registration/unsuccessful_login.html')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'registration/login.html', {'form': form})
+
+
+def logout_user(request):
+    logout(request)
+    return redirect('post_list')
