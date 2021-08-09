117cb425316ad39a967680b5253d97dee64eafb6
am2601 <artem.markovskiy2003@gmail.com>
2021-08-09 18:20:08 +0200

    little fixes 1.2

--- blog/forms.py
+++ blog/forms.py
@@ -6,7 +6,7 @@ from django.conf import settings
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ('title', 'text', 'image', 'image_alt')
+        fields = ('title', 'text', 'post_image', 'image_alt')
 
 
 class CommentForm(forms.ModelForm):
